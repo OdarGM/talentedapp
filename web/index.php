8985e23014753cbbe1cda8e7990e7da2af31880a
Jose Barranco <jdcoding01@gmail.com>
2018-06-23 12:50:06 -0400

    Update index.php

--- web/index.php
+++ web/index.php
@@ -16,7 +16,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="/stylesheets/style_main.css">
-	  <script src="/views/script_main.js"></script>
+	  
   </head>
   <body>
   <div class="topnav" id="myTopnav">
