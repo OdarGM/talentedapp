d1f106d956fbf66bbc0631f3feb9f556b92fd972
Jose Barranco <jdcoding01@gmail.com>
2018-06-23 12:36:55 -0400

    Update log.php

--- web/log.php
+++ web/log.php
@@ -1,10 +1,10 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'ec2-54-243-40-26.compute-1.amazonaws.com');
-define('DB_USERNAME', 'surujdrgkqyzxk');
-define('DB_PASSWORD', '7579fc29f93ec1538d6baf8683306a6c4fb7b106cb6ba1c15109bd8e75a2012a');
-define('DB_NAME', 'd4qss9hg17hij8');
+define('DB_SERVER', 'us-cdbr-iron-east-04.cleardb.net');
+define('DB_USERNAME', 'b3b6d5d1e98004');
+define('DB_PASSWORD', '30536bc2');
+define('DB_NAME', 'heroku_c3aabcc7808faa7');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
