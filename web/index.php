e039021ea93294d97ba425d4dd94d87555d1ddd4
Jose Barranco <jdcoding01@gmail.com>
2018-06-22 23:19:12 -0400

    Add files via upload

---
+++ web/index.php
@@ -0,0 +1,66 @@
+<!--TalentEd LogIn Template-->
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>TalentEd | LogIn</title>
+    <link rel="icon" href="https://cdn.glitch.com/2510be4b-441e-4689-8be2-29be95d39431%2Fnewlogo.png?1529281742869"> 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style>
+       body {background: url("https://www.freevector.com/uploads/vector/preview/9458/FreeVector-City-Skyscrapers-Buildings-Vector.jpg");}
+    </style>
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/static/style_main.css">
+	  <script src="../script_main.js"></script>
+  </head>
+  <body>
+  <div class="topnav" id="myTopnav">
+  <a href="../about.html" class="active">About</a>
+  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+    <i class="fa fa-bars"></i>
+  </a>
+</div>
+	  
+	  
+	  
+	  
+    <div class="content">
+      <div class="login-section">
+        <img src="https://cdn.glitch.com/2510be4b-441e-4689-8be2-29be95d39431%2Fnewlogo.png?1529281742869" class="pic">
+        <h1>TalentEd Login</h1>
+        <form>
+
+          <input type="password" value="Password" id="myInput">
+		<p>Show Password</p>
+          <input type="checkbox" onclick="myFunction()">
+<script>
+function myFunction() {
+    var x = document.getElementById("myInput");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
+</script><br>
+          <input type="text" name="email" placeholder="Talented ID">
+          <input type="submit" name="submit" href="loggedIn.html" value="Login">
+          <a href="lostPassword.php">Lost your password?</a><br>
+          <a href="../signup.html">Don't have an account?</a>
+          <center><p> &copy; 2018 TalentEd</p></center> 
+          <center><p> &copy; Powered by Inv Technologies.</p></center> 
+        </form>
+      </div>
+    </div>
+<?php
+$email = test_input($_POST["email"]);
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+  $emailErr = "Invalid email format"; 
+}
+?>
+  </body>
+</html>
