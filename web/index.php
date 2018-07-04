ea0f9824954d8cdb103af6f8e774d66a55d11ae7
Odar <radoradev770@gmail.com>
2018-07-05 00:23:44 +0300

    Update index.php

--- web/index.php
+++ web/index.php
@@ -121,7 +121,7 @@
 <ul>
    <li class='active'><a href='http://mytalented.com/'>Home</a></li>
    <li><a href='#'>Products</a></li>
-   <li><a href='http://mytalented.com/views/about.html'>About US</a></li>
+   <li><a href='http://www.mytalented.com/views/about.html'>About US</a></li>
    <li><a href='http://status.mytalented.com/'>Systems Status</a></li>
 </ul>
 </div>
